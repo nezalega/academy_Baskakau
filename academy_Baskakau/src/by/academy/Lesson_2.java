efe7501f4043f0e4ba241d41bae58e5576c6e2f1
nezalega <baskakoffoi@gmail.com>
2020-06-29 21:20:06 +0300

    Lesson2

---
+++ academy_Baskakau/src/by/academy/Lesson_2.java
@@ -0,0 +1,13 @@
+package by.academy;
+
+public class Lesson_2 {
+
+	public static void main (String [] args) {
+		
+		long a = 2123000l;
+		byte b = 100;
+		int c = 234;
+		
+		System.out.println("Otvet = " + a/b*c);
+	
+}}
---
+++ academy_Baskakau/src/by/academy/Task.java
@@ -0,0 +1,16 @@
+package by.academy;
+
+public class Task {
+
+	public static void main (String [] args) {
+		
+		int i = 2;
+		int j = 3;
+		int k = i+j;
+		
+		System.out.println("Otvet: 3*3 + 4*4=25 "); 
+		
+		System.out.println("Primer: " +  (j*j + "+" + i*i) + " = " + k);
+		
+	}
+}
